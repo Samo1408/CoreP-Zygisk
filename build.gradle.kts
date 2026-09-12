d65a2b7d24c0220c4edf1f086f7cbecb1f875965
Samo1408 <ab.0ss0mm@gmail.com>
2026-09-12 22:22:56 +0300

    Upload build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+import java.util.zip.ZipFile as JvmZipFile
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+}
+version = "2.0-zg"
