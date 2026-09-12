3616c260493d6c67e2a341649ae756e55581d8e5
Samo1408 <ab.0ss0mm@gmail.com>
2026-09-12 21:53:11 +0300

    Upload app/src/main/java/org/lsposed/corepatch/hook/ReconcilePackageUtilsHook.kt

---
+++ app/src/main/java/org/lsposed/corepatch/hook/ReconcilePackageUtilsHook.kt
@@ -0,0 +1,32 @@
+package org.lsposed.corepatch.hook
+
+import android.annotation.SuppressLint
+import android.os.Build
+import org.lsposed.corepatch.Config
+import org.lsposed.corepatch.ZygiskHelper
+import org.lsposed.corepatch.ZygiskHelper.hostClassLoader
+import org.lsposed.corepatch.ZygiskHelper.log
+import org.lsposed.corepatch.ZygiskHelper.setStaticBoolean
+
+object ReconcilePackageUtilsHook : BaseHook() {
+    override val name = "ReconcilePackageUtilsHook"
+
+    @SuppressLint("PrivateApi")
+    override fun hook() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) return
+
+        // https://cs.android.com/android/platform/superproject/+/android-14.0.0_r75:frameworks/base/services/core/java/com/android/server/pm/ReconcilePackageUtils.java
+        val reconcilePackageUtilsClazz =
+            hostClassLoader.loadClass("com.android.server.pm.ReconcilePackageUtils")
+        val reconcilePackagesMethod =
+            reconcilePackageUtilsClazz.declaredMethods.first { m -> m.name == "reconcilePackages" }
+        if (!ZygiskHelper.deoptimize(reconcilePackagesMethod)) log("failed to deoptimize reconcilePackages")
+
+        if (Config.isBypassDigestEnabled() && Config.isBypassSharedUserEnabled()) {
+            reconcilePackageUtilsClazz.declaredFields.firstOrNull { field -> field.name == "ALLOW_NON_PRELOADS_SYSTEM_SHAREDUIDS" }
+                ?.let { field ->
+                    setStaticBoolean(field, true)
+                }
+        }
+    }
+}
