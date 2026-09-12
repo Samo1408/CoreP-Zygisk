88dc1a44d1daebe0aa717f3a3768fa118ca7ba29
Samo1408 <ab.0ss0mm@gmail.com>
2026-09-12 22:00:57 +0300

    Upload app/src/main/java/org/lsposed/corepatch/hook/ApkSigningBlockUtilsHook.kt

---
+++ app/src/main/java/org/lsposed/corepatch/hook/ApkSigningBlockUtilsHook.kt
@@ -0,0 +1,32 @@
+package org.lsposed.corepatch.hook
+
+import android.annotation.SuppressLint
+import org.lsposed.corepatch.Config
+import org.lsposed.corepatch.ZygiskHelper.hookBefore
+import org.lsposed.corepatch.ZygiskHelper.hostClassLoader
+
+object ApkSigningBlockUtilsHook : BaseHook() {
+    override val name = "ApkSigningBlockUtilsHook"
+
+    @SuppressLint("PrivateApi")
+    override fun hook() {
+        val apkSigningBlockUtilsClazz =
+            hostClassLoader.loadClass("android.util.apk.ApkSigningBlockUtils")
+        // https://cs.android.com/android/platform/superproject/+/android-9.0.0_r61:frameworks/base/core/java/android/util/apk/ApkSigningBlockUtils.java;l=303
+        val parseVerityDigestAndVerifySourceLengthMethod =
+            apkSigningBlockUtilsClazz.declaredMethods.first { m -> m.name == "parseVerityDigestAndVerifySourceLength" }
+        hookBefore(parseVerityDigestAndVerifySourceLengthMethod) { callback ->
+            if (Config.isBypassVerificationEnabled()) {
+                callback.returnAndSkip((callback.args[0] as ByteArray).copyOfRange(0, 32))
+            }
+        }
+
+        val verifyIntegrityForVerityBasedAlgorithmMethod =
+            apkSigningBlockUtilsClazz.declaredMethods.first { m -> m.name == "verifyIntegrityForVerityBasedAlgorithm" }
+        hookBefore(verifyIntegrityForVerityBasedAlgorithmMethod) { callback ->
+            if (Config.isBypassVerificationEnabled()) {
+                callback.returnAndSkip(null)
+            }
+        }
+    }
+}
