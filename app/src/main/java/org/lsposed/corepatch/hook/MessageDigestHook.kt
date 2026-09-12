01b49660a728d745911397ea79eb2a90c85d6c28
Samo1408 <ab.0ss0mm@gmail.com>
2026-09-12 22:22:18 +0300

    Upload app/src/main/java/org/lsposed/corepatch/hook/MessageDigestHook.kt

---
+++ app/src/main/java/org/lsposed/corepatch/hook/MessageDigestHook.kt
@@ -0,0 +1,23 @@
+package org.lsposed.corepatch.hook
+
+import org.lsposed.corepatch.Config
+import org.lsposed.corepatch.ZygiskHelper
+import org.lsposed.corepatch.ZygiskHelper.hostClassLoader
+
+object MessageDigestHook : BaseHook() {
+    override val name = "MessageDigestHook"
+
+    override fun hook() {
+        val messageDigestClazz = hostClassLoader.loadClass("java.security.MessageDigest")
+        // https://cs.android.com/android/platform/superproject/main/+/main:libcore/ojluni/src/main/java/java/security/MessageDigest.java;l=518
+        // public static boolean isEqual(byte[] digesta, byte[] digestb)
+        val isEqualMethod = messageDigestClazz.getDeclaredMethod(
+            "isEqual", ByteArray::class.java, ByteArray::class.java
+        )
+        ZygiskHelper.hookBefore(isEqualMethod) { callback ->
+            if (Config.isBypassVerificationEnabled()) {
+                callback.returnAndSkip(true)
+            }
+        }
+    }
+}
