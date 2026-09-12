d33637ca2d738a6624332c4306e4cf81fa93dfd9
Samo1408 <ab.0ss0mm@gmail.com>
2026-09-12 22:22:22 +0300

    Upload app/src/main/java/org/lsposed/corepatch/hook/VerificationParamsHook.kt

---
+++ app/src/main/java/org/lsposed/corepatch/hook/VerificationParamsHook.kt
@@ -0,0 +1,27 @@
+package org.lsposed.corepatch.hook
+
+import android.annotation.SuppressLint
+import android.os.Build
+import org.lsposed.corepatch.Config
+import org.lsposed.corepatch.ZygiskHelper.hookBefore
+import org.lsposed.corepatch.ZygiskHelper.hostClassLoader
+
+object VerificationParamsHook: BaseHook() {
+    override val name = "VerificationParamsHook"
+
+    @SuppressLint("PrivateApi")
+    override fun hook() {
+        if (Build.VERSION.SDK_INT != Build.VERSION_CODES.TIRAMISU) {
+            return
+        }
+
+        val verificationParamsClazz = hostClassLoader.loadClass("com.android.server.pm.VerificationParams")
+
+        val isVerificationEnabledMethod = verificationParamsClazz.declaredMethods.first { m -> m.name == "isVerificationEnabled" }
+        hookBefore(isVerificationEnabledMethod) { callback ->
+            if (Config.isDisableVerificationAgentEnabled()) {
+                callback.returnAndSkip(false)
+            }
+        }
+    }
+}
