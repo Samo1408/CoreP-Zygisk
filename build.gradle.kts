3b966206e85255b1ac004de0f6a87e6dc41942b8
Samo1408 <ab.0ss0mm@gmail.com>
2026-09-12 22:21:23 +0300

    Delete build.gradle.kts

--- build.gradle.kts
+++
@@ -1,6 +0,0 @@
-import java.util.zip.ZipFile as JvmZipFile
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    alias(libs.plugins.android.application) apply false
-}
-version = "2.0-zg"
